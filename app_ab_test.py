b38810a5cb93c8f1d750f6ce36aad671a582e378
pdesj <paul.desjardin1@gmail.com>
2023-03-14 21:40:33 +0100

    [feature] Minor changes in displayed data

--- app_ab_test.py
+++ app_ab_test.py
@@ -62,7 +62,7 @@ converted_a = st.sidebar.number_input(
     value=DEFAULT_INPUT["converted_a"],
 )
 converted_b = st.sidebar.number_input(
-    "Number of samples converted in group A",
+    "Number of samples converted in group B",
     min_value=0,
     max_value=nb_a,
     value=DEFAULT_INPUT["converted_b"],
@@ -83,6 +83,11 @@ samples_conversion_rate_b = np.random.beta(
 
 diff = samples_conversion_rate_a - samples_conversion_rate_b
 
+# Convert in percentage
+samples_conversion_rate_a *= 100
+samples_conversion_rate_b *= 100
+diff *= 100
+
 
 # Plot results
 fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(12, 8))
@@ -90,14 +95,14 @@ fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(12, 8))
 sns.histplot(samples_conversion_rate_a, stat="percent", alpha=0.5, kde=True, ax=axs[0])
 sns.histplot(samples_conversion_rate_b, stat="percent", alpha=0.5, kde=True, ax=axs[0])
 
-axs[0].set_xlabel("Conversion rate")
+axs[0].set_xlabel("Conversion rate (%)")
 axs[0].set_ylabel("Probability density (%)")
 axs[0].set_title("Distribution of conversion rate between A and B groups")
 axs[0].legend(["A", "B"])
 
 sns.histplot(diff, stat="percent", alpha=0.5, kde=True, ax=axs[1])
 
-axs[1].set_xlabel("Conversion rate")
+axs[1].set_xlabel("Absolute difference in conversion rate (%)")
 axs[1].set_ylabel("Probability density (%)")
 axs[1].set_title("Distribution of the difference in conversion rate between A and B groups")
 
@@ -125,14 +130,14 @@ summary = f"""
 - Difference in conversion rate: {converted_a / nb_a - converted_b / nb_b:.1%} 
 
 #### Estimation of variability
-- Probability of group A's conversion rate being higher than group B's conversion rate: {(diff > 0).mean():.1%}
-- There is 90% chances that group A's conversion rate minus groupb B's conversion high is higher than {np.percentile(diff, 10):.1%}
-- There is 95% chances that group A's conversion rate minus groupb B's conversion high is higher than {np.percentile(diff, 5):.1%}
-- There is 99% chances that group A's conversion rate minus groupb B's conversion high is higher than {np.percentile(diff, 1):.1%}
+- Probability of group A's conversion rate being higher than group B's conversion rate: {(diff > 0).mean():.1f}%
+- There is 90% chances that group A's conversion rate minus groupb B's conversion high is higher than {np.percentile(diff, 10):.1f}%
+- There is 95% chances that group A's conversion rate minus groupb B's conversion high is higher than {np.percentile(diff, 5):.1f}%
+- There is 99% chances that group A's conversion rate minus groupb B's conversion high is higher than {np.percentile(diff, 1):.1f}%
 """
 
 st.markdown(summary)
 st.subheader("Conversion rates by centile")
-st.dataframe(df_repartitions)
+st.dataframe(df_repartitions.round(1))
 st.subheader("Parameters' distribution")
 st.pyplot(fig)
